e81700dc5aa02ee121e24495593226e9336aca17
Michael d <Michael d>
2016-09-07 12:28:55 +0200

    fixed wshttp binding and added wcf_client

---
+++ WCF_Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCF_Client.MovieServiceReference;
+
+namespace WCF_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press <Enter> to run the client...");
+            Console.ReadLine();
+
+        
+            
+                var client = new MovieServiceClient("WSHttpBinding_IMovieService");
+                foreach (var movies in client.GetMovies())
+                {
+                    Console.WriteLine(movies.Title + " / " + movies.Rating);
+                }
+
+                Console.ReadLine();
+            
+
+        }
+    }
+}
+
